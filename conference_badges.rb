409adccadc06bf3f5720d7b284e9028ca41076de
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-09 14:47:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,8 @@ def printer(array)
   var1 = batch_badge_creator(array)
   var2 = assign_rooms(array)
   var.each_with_index do |name, i|
-
+    puts var1[i]
+    puts var2[i]
   end
   rooms = assign_rooms(array)
   rooms.each do |room|
